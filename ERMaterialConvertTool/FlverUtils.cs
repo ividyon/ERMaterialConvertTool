6b91cbaa112bbac0b62978a1566feb4bac762a34
ividyon <1077140+ividyon@users.noreply.github.com>
2025-08-20 21:31:24 +0200

    Initial commit

---
+++ ERMaterialConvertTool/FlverUtils.cs
@@ -0,0 +1,139 @@
+﻿using System.Numerics;
+using SoulsFormats;
+
+namespace ERMaterialSwapTool;
+
+public static class FlverUtils
+{
+
+
+    public static void PadVertex(FLVER.Vertex vertex, IEnumerable<FLVER2.BufferLayout> bufferLayouts)
+    {
+        Dictionary<FLVER.LayoutSemantic, int> usageCounts = new();
+        FLVER.LayoutSemantic[] paddedProperties =
+            { FLVER.LayoutSemantic.Tangent, FLVER.LayoutSemantic.UV, FLVER.LayoutSemantic.VertexColor };
+
+        IEnumerable<FLVER.LayoutMember> layoutMembers = bufferLayouts.SelectMany(bufferLayout => bufferLayout)
+            .Where(x => paddedProperties.Contains(x.Semantic));
+        foreach (FLVER.LayoutMember layoutMember in layoutMembers)
+        {
+            bool isDouble = layoutMember is
+            {
+                Semantic: FLVER.LayoutSemantic.UV,
+                Type: FLVER.LayoutType.Float4 or
+                FLVER.LayoutType.Short4 or
+                FLVER.LayoutType.Half2 or
+                FLVER.LayoutType.UByte4Norm
+            };
+            int count = isDouble ? 2 : 1;
+
+            if (usageCounts.ContainsKey(layoutMember.Semantic))
+            {
+                usageCounts[layoutMember.Semantic] += count;
+            }
+            else
+            {
+                usageCounts.Add(layoutMember.Semantic, count);
+            }
+        }
+
+        if (usageCounts.TryGetValue(FLVER.LayoutSemantic.Tangent, out int tangentCount))
+        {
+            int missingTangentCount = tangentCount - vertex.Tangents.Count;
+            var replaceTangent = Vector4.Zero;
+            if (vertex.Tangents.Count > 0)
+                replaceTangent = vertex.Tangents.First();
+            for (int i = 0; i < missingTangentCount; i++)
+            {
+                vertex.Tangents.Add(replaceTangent);
+            }
+        }
+
+        if (usageCounts.TryGetValue(FLVER.LayoutSemantic.UV, out int uvCount))
+        {
+            int missingUvCount = uvCount - vertex.UVs.Count;
+            var replaceUv = Vector3.Zero;
+            if (vertex.UVs.Count > 0)
+                replaceUv = vertex.UVs.First();
+            for (int i = 0; i < missingUvCount; i++)
+            {
+                vertex.UVs.Add(replaceUv);
+            }
+        }
+
+        if (usageCounts.TryGetValue(FLVER.LayoutSemantic.VertexColor, out int colorCount))
+        {
+            int missingColorCount = colorCount - vertex.Colors.Count;
+            for (int i = 0; i < missingColorCount; i++)
+            {
+                vertex.Colors.Add(new FLVER.VertexColor(255, 255, 0, 0));
+            }
+        }
+    }
+
+    public static List<int> GetLayoutIndices(FLVER2 flver, List<FLVER2.BufferLayout> bufferLayouts)
+    {
+        List<int> indices = new();
+
+        foreach (FLVER2.BufferLayout referenceBufferLayout in bufferLayouts)
+        {
+            if (flver.BufferLayouts.Count == 0)
+            {
+                indices.Add(0);
+                flver.BufferLayouts.Add(referenceBufferLayout);
+            }
+            else
+            {
+                for (int i = 0; i < flver.BufferLayouts.Count; i++)
+                {
+                    FLVER2.BufferLayout bufferLayout = flver.BufferLayouts[i];
+                    if (bufferLayout.SequenceEqual(referenceBufferLayout, new LayoutMemberComparer()))
+                    {
+                        indices.Add(i);
+                        break;
+                    }
+
+                    if (i != flver.BufferLayouts.Count - 1) continue;
+
+                    indices.Add(i + 1);
+                    flver.BufferLayouts.Add(referenceBufferLayout);
+                    break;
+                }
+            }
+        }
+
+        return indices;
+    }
+
+    public static void AdjustBoneIndexBufferSize(FLVER2 flver, List<FLVER2.BufferLayout> bufferLayouts)
+    {
+        if (flver.Nodes.FindLastIndex(x => !x.Flags.HasFlag(FLVER.Node.NodeFlags.Disabled)) <= byte.MaxValue) return;
+
+        foreach (FLVER2.BufferLayout bufferLayout in bufferLayouts)
+        {
+            foreach (FLVER.LayoutMember layoutMember in bufferLayout.Where(x =>
+                         x.Semantic == FLVER.LayoutSemantic.BoneIndices))
+            {
+                layoutMember.Type = FLVER.LayoutType.UShort4;
+            }
+        }
+    }
+
+    private class LayoutMemberComparer : IEqualityComparer<FLVER.LayoutMember>
+    {
+        public bool Equals(FLVER.LayoutMember? x, FLVER.LayoutMember? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (x is null) return false;
+            if (y is null) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Stream == y.Stream && x.Type == y.Type && x.Semantic == y.Semantic && x.Index == y.Index &&
+                   x.Size == y.Size;
+        }
+
+        public int GetHashCode(FLVER.LayoutMember obj)
+        {
+            return HashCode.Combine(obj.Stream, (int)obj.Type, (int)obj.Semantic, obj.Index, obj.Size);
+        }
+    }
+}
---
+++ ERMaterialConvertTool/Program.cs
@@ -0,0 +1,280 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Globalization;
+using PPlus;
+using SoulsAssetPipeline.FLVERImporting;
+using SoulsFormats;
+
+namespace ERMaterialSwapTool;
+
+class Program
+{
+    private enum Mode
+    {
+        [Display(Name = "Convert FLVER from another game")]
+        ConvertFlver,
+
+        [Display(Name = "Swap a material for another")]
+        MaterialSwap,
+    }
+
+    public static bool IsDebug()
+    {
+        return Debugger.IsAttached;
+    }
+
+    private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        var exception = e.ExceptionObject as Exception;
+        File.WriteAllText(Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory)!, "crash.log"),
+            exception?.InnerException?.ToString() ?? exception?.ToString());
+        throw exception;
+    }
+
+    private static void RunProgram(string[] args)
+    {
+        PromptPlus.DoubleDash("ERMaterialConvertTool");
+        PromptPlus.WriteLine(
+            "Hi! This is a quick tool for changing materials in an ELDEN RING FLVER2 file,\nor converting FLVER2 files from other games to ELDEN RING.");
+        PromptPlus.WriteLine();
+
+        var modeSelect = PromptPlus.Select<Mode>("Select mode of operation").Run();
+        if (modeSelect.IsAborted) return;
+
+        var mode = modeSelect.Value;
+
+        PromptPlus.KeyPress("In the next dialog, please select the FLVER file").Run();
+        string filePath;
+        while (true)
+        {
+            var picker = NativeFileDialogSharp.Dialog.FileOpen();
+            if (!picker.IsOk)
+            {
+                PromptPlus.KeyPress("Invalid file path! Try again...").Run();
+                continue;
+            }
+
+            filePath = picker.Path;
+            break;
+        }
+
+        PromptPlus.WriteLine("Loading FLVER...");
+        var flver = FLVER2.Read(filePath);
+
+        PromptPlus.WriteLine("Loading material bank...");
+        string matInfoPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SapResources",
+            "FLVER2MaterialInfoBank", "BankER.xml");
+        var matInfoBank = FLVER2MaterialInfoBank.ReadFromXML(matInfoPath);
+
+        bool changesMade = false;
+        bool exitAndSave = false;
+        if (mode == Mode.ConvertFlver)
+        {
+            PromptPlus.WriteLine("Preparing FLVER...");
+            var flverVersion = flver.Header.Version;
+            bool isPreEldenRing = flverVersion < 0x2001A;
+            bool isNightreign = flverVersion == 0x20021;
+            flver.Header.Version = 0x2001A;
+            if (isPreEldenRing)
+            {
+                PromptPlus.WriteLine("FLVER is pre-ELDEN RING; adding a skeleton set.");
+                flver.Skeletons = new FLVER2.SkeletonSet();
+            }
+
+            if (isNightreign)
+            {
+                PromptPlus.WriteLine("FLVER is from Nightreign; changing some header values.");
+                flver.Header.Unk68 = 4;
+            }
+
+            flver.BufferLayouts = new();
+            flver.GXLists = new();
+
+            Dictionary<string, FLVER2MaterialInfoBank.MaterialDef> mapping = new();
+            PromptPlus.WriteLine(
+                $"Materials:\n{string.Join("\n", flver.Materials.Select(a => $"#{flver.Materials.IndexOf(a)}: {a.Name} ({a.MTD}) Index {a.Index}"))}\n");
+            foreach (string mtd in flver.Materials.Select(a => Path.GetFileNameWithoutExtension(a.MTD).ToLower())
+                         .Distinct())
+            {
+                var select = PromptPlus.Select<FLVER2MaterialInfoBank.MaterialDef>($"Select ER material to map to {mtd}")
+                    .TextSelector(a => $"{a.MTD} ({a.Shader})")
+                    .AddItems(matInfoBank.MaterialDefs.Values);
+
+                var selectPrompt = select.Run();
+
+                mapping[mtd] = selectPrompt.Value;
+            }
+
+            foreach (FLVER2.Material flverMaterial in flver.Materials)
+            {
+                var matIndex = flver.Materials.IndexOf(flverMaterial);
+                PromptPlus.WriteLine(
+                    $"Processing material #{matIndex}: {Path.GetFileNameWithoutExtension(flverMaterial.Name)}");
+                var matName = Path.GetFileNameWithoutExtension(flverMaterial.MTD).ToLower();
+
+                FLVER2MaterialInfoBank.MaterialDef matDef = mapping[matName];
+
+                FLVER2.GXList gxList = new();
+                gxList.AddRange(matInfoBank.GetDefaultGXItemsForMTD(matDef.MTD));
+                flver.GXLists.Add(gxList);
+                flverMaterial.MTD = matDef.MTD;
+                flverMaterial.GXIndex = flver.GXLists.IndexOf(gxList);
+
+                flverMaterial.Textures =
+                    matDef.TextureChannels.Values.Select(x => new FLVER2.Texture { ParamName = x }).ToList();
+
+                var meshes = flver.Meshes.Where(a => a.MaterialIndex == matIndex).ToList();
+                var firstMesh = meshes.First();
+                var acceptableBufferDeclarations = matDef.AcceptableVertexBufferDeclarations;
+                List<FLVER2.BufferLayout> bufferLayouts = acceptableBufferDeclarations[0].Buffers;
+                if (acceptableBufferDeclarations.Count > 1)
+                {
+                    List<FLVER2.BufferLayout>? matchingLayouts = acceptableBufferDeclarations.FirstOrDefault(x =>
+                        x.Buffers.SelectMany(y => y).Count(y => y.Semantic == FLVER.LayoutSemantic.Tangent) >=
+                        firstMesh.Vertices.First().Tangents.Count)?.Buffers;
+
+                    if (matchingLayouts != null)
+                    {
+                        // Log.Log("Replace with matching layouts");
+                        bufferLayouts = matchingLayouts;
+                    }
+                }
+
+                List<int> layoutIndices = FlverUtils.GetLayoutIndices(flver, bufferLayouts);
+                foreach (FLVER2.Mesh mesh in meshes)
+                {
+                    mesh.VertexBuffers = layoutIndices.Select(x => new FLVER2.VertexBuffer(x)).ToList();
+                    foreach (var v in mesh.Vertices)
+                    {
+                        FlverUtils.PadVertex(v, bufferLayouts);
+                    }
+                }
+
+                FlverUtils.AdjustBoneIndexBufferSize(flver, bufferLayouts);
+            }
+
+            PromptPlus.WriteLine();
+            PromptPlus.WriteLine($"FLVER conversion complete.");
+            PromptPlus.WriteLine();
+            exitAndSave = true;
+        }
+        else if (mode == Mode.MaterialSwap)
+        {
+            while (true)
+            {
+                if (changesMade)
+                {
+                    var confirm = PromptPlus.Confirm("Swap more materials? (Select \"No\" to save your changes)").Run();
+                    if (confirm.Value.IsNoResponseKey())
+                    {
+                        exitAndSave = true;
+                        break;
+                    }
+                }
+
+                var matSelect = PromptPlus
+                    .Select<FLVER2.Material>("Select a material to swap")
+                    .AddItems(flver.Materials)
+                    .TextSelector(m => $"#{flver.Materials.IndexOf(m)}: {m.Name} || {m.MTD}")
+                    .Run();
+                if (matSelect.IsAborted) break;
+                var material = matSelect.Value;
+                int matIdx = flver.Materials.IndexOf(material);
+                PromptPlus.WriteLine(
+                    $"Selected #{flver.Materials.IndexOf(material)}: {material.Name} || {material.MTD}");
+                var select = PromptPlus
+                    .Select<FLVER2MaterialInfoBank.MaterialDef>($"Select new material to replace {material.Name} with")
+                    .TextSelector(a => $"{a.MTD} ({a.Shader})")
+                    .AddItems(matInfoBank.MaterialDefs.Values)
+                    .Run();
+                if (select.IsAborted) break;
+                var replaceMatDef = select.Value;
+
+                FLVER2.GXList gxList = new();
+                gxList.AddRange(matInfoBank.GetDefaultGXItemsForMTD(replaceMatDef.MTD));
+                flver.GXLists.Add(gxList);
+                material.MTD = replaceMatDef.MTD;
+                material.GXIndex = flver.GXLists.IndexOf(gxList);
+
+                material.Textures =
+                    replaceMatDef.TextureChannels.Values.Select(x => new FLVER2.Texture { ParamName = x }).ToList();
+
+                var meshes = flver.Meshes.Where(a => a.MaterialIndex == matIdx).ToList();
+                var firstMesh = meshes.First();
+                var acceptableBufferDeclarations = replaceMatDef.AcceptableVertexBufferDeclarations;
+                List<FLVER2.BufferLayout> bufferLayouts = acceptableBufferDeclarations[0].Buffers;
+                if (acceptableBufferDeclarations.Count > 1)
+                {
+                    List<FLVER2.BufferLayout>? matchingLayouts = acceptableBufferDeclarations.FirstOrDefault(x =>
+                        x.Buffers.SelectMany(y => y).Count(y => y.Semantic == FLVER.LayoutSemantic.Tangent) >=
+                        firstMesh.Vertices.First().Tangents.Count)?.Buffers;
+
+                    if (matchingLayouts != null)
+                    {
+                        // Log.Log("Replace with matching layouts");
+                        bufferLayouts = matchingLayouts;
+                    }
+                }
+
+                List<int> layoutIndices = FlverUtils.GetLayoutIndices(flver, bufferLayouts);
+                foreach (FLVER2.Mesh mesh in meshes)
+                {
+                    mesh.VertexBuffers = layoutIndices.Select(x => new FLVER2.VertexBuffer(x)).ToList();
+                    foreach (var v in mesh.Vertices)
+                    {
+                        FlverUtils.PadVertex(v, bufferLayouts);
+                    }
+                }
+
+                FlverUtils.AdjustBoneIndexBufferSize(flver, bufferLayouts);
+                changesMade = true;
+                PromptPlus.WriteLine();
+                PromptPlus.WriteLine($"Material conversion complete.");
+                PromptPlus.WriteLine();
+            }
+        }
+
+        if (exitAndSave)
+        {
+            PromptPlus.WriteLine("Backing up FLVER...");
+            if (File.Exists(filePath))
+            {
+                File.Copy(filePath, ($"{filePath}.bak"), true);
+            }
+
+            PromptPlus.WriteLine("Saving FLVER...");
+            flver.Write(filePath);
+            PromptPlus.KeyPress("Successfully saved the FLVER! Enjoy.").Run();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        PromptPlus.Config.DefaultCulture = new CultureInfo("en-us");
+        PromptPlus.IgnoreColorTokens = true;
+
+        if (!IsDebug())
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+
+        try
+        {
+            RunProgram(args);
+        }
+        catch (Exception e) when (!IsDebug())
+        {
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), "crash.log"),
+                e?.InnerException?.ToString() ?? e?.ToString());
+            PromptPlus.Error.WriteLine(@$"
+There was an exception:
+
+{e?.InnerException?.ToString() ?? e?.ToString()}
+
+This error message has also been saved to crash.log in the program directory.
+
+Press any key to exit...");
+            PromptPlus.ReadKey();
+        }
+    }
+}
